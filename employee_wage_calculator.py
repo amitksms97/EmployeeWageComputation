fc74f92c10ea63ccb2aeea4fae6f9306bc3e4c51
amtksms97 <amtksms97@gmail.com>
2021-01-10 21:41:10 +0530

    Added : Code to to calculate daily wage of the employee

--- employee_wage_calculator.py
+++ employee_wage_calculator.py
@@ -1,9 +1,12 @@
 import random
 
 print("Welcome to Employee Wage Computation Program")
-attendance = random.randint(0, 2)
+WAGE_PER_HOUR = 20
+FULL_DAY_HOUR = 8
+attendance = random.randint(0, 1)
 if attendance == 0:
     print("Absent")
 elif attendance == 1:
     print("Present")
-
+employee_wage = attendance * WAGE_PER_HOUR * FULL_DAY_HOUR
+print("Employee Wage  is equal to {}".format(employee_wage))
