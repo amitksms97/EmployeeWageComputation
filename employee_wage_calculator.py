d54ddb0f98ac8f045e0c78ec65fc07be70ff1c6e
amtksms97 <amtksms97@gmail.com>
2021-01-10 19:24:31 +0530

    Added : Code to to check whether the employee was present or absent

--- employee_wage_calculator.py
+++ employee_wage_calculator.py
@@ -1,1 +1,9 @@
+import random
+
 print("Welcome to Employee Wage Computation Program")
+attendance = random.randint(0, 2)
+if attendance == 0:
+    print("Absent")
+elif attendance == 1:
+    print("Present")
+
